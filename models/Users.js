cbb7a0e74d71bed553e92734ca264573cb3b19f2
Liane.Andriatsilavo <landriatsilavo@gmail.com>
2026-01-30 20:03:55 +0300

    test connexion bdd et première entité avec crud

---
+++ models/Users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UsersSchema = new mongoose.Schema({
+  id: { type: String, required: false },       
+  mdp: { type: String, required: true },            
+  prenom: { type: String, required: true },         
+  nom: { type: String, required: true },            
+  phone: { type: String, required: false },         
+//   role_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Roles', required: true },
+//   statut_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Statuts', required: true }, 
+  created_on: { type: Date, default: Date.now },    
+  updated_at: { type: Date, default: Date.now },
+  pdp: { type: String, required: false },         
+  adresse: { type: String, required: false },         
+  email: { type: String, required: true }       
+}, { timestamps: false });
+
+module.exports = mongoose.model('Users', UsersSchema);

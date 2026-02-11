0bc70abd445df0260a5a8cc0a75e6f6bb5d23f09
Amboara <amboaratsarapro@gmail.com>
2026-02-11 21:16:08 +0300

    dashboard-user misy bug 2

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,23 +3,17 @@ import { LoginComponent } from './components/login/login';
 import { ClientDashboard } from './components/client-dashboard/client-dashboard';
 import { ProfileComponent } from './pages/profile/profile';
 import { ShopDashboard } from './components/shop-dashboard/shop-dashboard';
-<<<<<<< Updated upstream
-import { ProduitsMagasinComponent } from './components/produits-magasin/produits-magasin';
-=======
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
->>>>>>> Stashed changes
+import { ProduitsMagasinComponent } from './components/produits-magasin/produits-magasin';
+
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'client-dashboard', component: ClientDashboard },
   { path: 'admin-dashboard', component: AdminDashboardComponent },
   { path: 'shop-dashboard', component: ShopDashboard },
   { path: 'profile', component: ProfileComponent },
-<<<<<<< Updated upstream
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'magasin/:id/produits', component: ProduitsMagasinComponent }
   
 ];
-=======
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
-];
->>>>>>> Stashed changes
+

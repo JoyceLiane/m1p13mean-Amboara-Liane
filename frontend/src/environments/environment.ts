eeae68c97a920e45607459e71af0f0bbfedc8170
Amboara <amboaratsarapro@gmail.com>
2026-02-17 18:57:31 +0300

    environnement.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://m1p13mean-amboara-liane.onrender.com',
-  // apiUrl: 'http://localhost:5000',
+  // apiUrl: 'https://m1p13mean-amboara-liane.onrender.com',
+  apiUrl: 'http://localhost:5000',
   uploadsUrl: 'http://localhost:5000/uploads'
 }

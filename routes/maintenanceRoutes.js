849c6befce536287f7f39fa7ae3659439eaed228
Amboara <amboaratsarapro@gmail.com>
2026-02-17 17:50:07 +0300

    update maintenance

--- routes/maintenanceRoutes.js
+++ routes/maintenanceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const DemandeMaintenance = require('../models/DemandeMaintenance');
+const DemandeMaintenance = require('../models/Maintenance');
 
 // CREATE
 router.post('/', async (req, res) => {

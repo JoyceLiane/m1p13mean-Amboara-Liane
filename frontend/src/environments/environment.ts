b73b6eac79d472af1849f7f6c6352971cfd33c7a
Amboara <amboaratsarapro@gmail.com>
2026-02-26 16:16:58 +0300

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://m1p13mean-amboara-liane.onrender.com',
-  apiUrl: 'http://localhost:5000',
-  uploadsUrl: 'http://localhost:5000/uploads'
+  apiUrl: 'https://m1p13mean-amboara-liane.onrender.com',
+  // apiUrl: 'http://localhost:5000',
+  uploadsUrl: 'https://m1p13mean-amboara-liane.onrender.com/uploads'
 }
